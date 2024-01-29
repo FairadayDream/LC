d3df111d250b0c8aac5d16a3ff7b20e29b4b1629
millerabigail266 <millerabigail266@gmail.com>
2024-01-29 11:57:17 -0600

    first commit

---
+++ leetcode53.js
@@ -0,0 +1,26 @@
+/* Given an integer array nums, find the subarray with the largest sum, and return its sum. */
+function newfunc() {
+  let subArray = [];
+  for (let i = 0; i <= subArray.length; i++) {
+    if (nums[0] + nums[1]) {
+    }
+  }
+  return nums;
+}
+
+// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+// Output: 6
+// Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+//Input:
+//let nums = [1] // Output: 1
+// Explanation: The subarray [1] has the largest sum 1.
+
+let nums = [5, 4, -1, 7, 8]; // Output: 23 // Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+console.log(newfunc(nums));
+
+// Constraints:
+// 1 <= nums.length <= 105
+// -104 <= nums[i] <= 104
+
+/* Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle. */
