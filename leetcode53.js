fc2d2466e74f5d042cbaece2d8156468b86c5912
millerabigail266 <millerabigail266@gmail.com>
2024-01-30 17:42:13 -0600

    addtyl

--- leetcode53.js
+++ leetcode53.js
@@ -1,23 +1,36 @@
+/*helper func - example
+data input transform - const words = const splitWord = input.split(' ');
+Output variable initialization - const answer = [];
+Base Case - if (splitWord.length === 0) return answer;
+whats the energy flow/action, - for loop - for(let i = 0; i < num; i++)
+whats the rock/conditions statement - let el = array.pop();
+Falsey statement - if (el) answer.push(el);
+Output transform- answer.join('');`
+*/
+
 /* Given an integer array nums, find the subarray with the largest sum, and return its sum. */
-function newfunc() {
-  let subArray = [];
-  for (let i = 0; i <= subArray.length; i++) {
-    if (nums[0] + nums[1]) {
+let nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+function func(nums) {
+  if (nums.length === 0) return 0;
+  for (let i = 1; i <= nums.length; i++) {
+    let ele = nums[0] + nums[1];
+
+    if (i === nums.length) {
+      return i;
     }
   }
-  return nums;
+  return maxSum;
 }
 
-// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
 // Output: 6
 // Explanation: The subarray [4,-1,2,1] has the largest sum 6.
-
-//Input:
-//let nums = [1] // Output: 1
+console.log(func([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
+// Output: 1
 // Explanation: The subarray [1] has the largest sum 1.
-
-let nums = [5, 4, -1, 7, 8]; // Output: 23 // Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
-console.log(newfunc(nums));
+console.log(func([1]));
+// Output: 23
+// Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+console.log(func([5, 4, -1, 7, 8]));
 
 // Constraints:
 // 1 <= nums.length <= 105
